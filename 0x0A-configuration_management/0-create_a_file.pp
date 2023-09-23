cdc017481d972fda6529e04fb16c912b69c67b33
Pritchad25 <pricts29@gmail.com>
2023-09-23 15:24:50 -0700

    Remove characters to make code line less or equal to 80 characters.

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-# This puppet manifest creates a file with specific permissions,
-owner, group, and content.
+# This puppet manifest creates a file with specific permissions.
 
 file { '/tmp/school':
-	ensure => 'file',
-	mode   => '0744',
-	owner  => 'www-data',
-	group  => 'www-data',
-	content => 'I love Puppet',
+ensure => 'file',
+mode   => '0744',
+owner  => 'www-data',
+group  => 'www-data',
+content => 'I love Puppet',
 }
