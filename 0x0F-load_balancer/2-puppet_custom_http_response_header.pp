ef76570b689a48d34766e47bb6be7111142f3ffc
Pritchad25 <pricts29@gmail.com>
2023-10-03 15:31:29 -0700

    configures a brand new Ubuntu machine to automate the task of creating a custom HTTP header response, but with Puppet.

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,29 @@
+# This script installs and configures a Nginx server using Puppet
+package { 'nginx':
+	ensure => 'installed',
+}
+
+file { '/var/www/html/index.html':
+	ensure  => 'file',
+	content => 'Hello World!',
+	require => Package['nginx'],
+}
+
+# The code below ensures that the Nginx service is running and enabled
+service { 'nginx':
+	ensure  => 'running',
+	enable  => true,
+	require => Package['nginx'],
+}
+
+file_line { 'add_header':
+	ensure  => 'present',
+	path    => '/etc/nginx/sites-available/default',
+	line    => "add_header X-Served-By ${hostname};",
+	after   => 'server_name _;',
+	require => Service['nginx'],
+}
+
+exec { 'restart nginx':
+	command     => '/usr/sbin/service nginx restart',
+}
