323386ef71ad482edec23c5dfd4f0c96506d9097
Pritchad25 <pricts29@gmail.com>
2023-11-07 14:39:10 +0000

    Fixing the issue causing the Apache to return 500 error.

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,19 @@
+# Fixing the issue that causes Apache to return a 500 error
+file { '/etc/profile.d/custom_ld_library_path.sh':
+
+ensure => present,
+
+content => 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/path/to/libfoo',
+
+mode => '0644'
+
+}
+# Restarting the Apache Service
+
+service { 'apache2':
+
+ensure => running,
+
+subscribe => File['/etc/profile.d/custom_ld_library_path.sh'],
+
+}
